743a2a22197e3abfb10aa0c810a4e740e56d8d56
Pranaya Joshi WSC <joship@wsc.school.nz>
2026-05-26 22:24:54 +1200

    Building home page

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,23 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 };
+
+
+// for window scroll 
+
+window.onscroll = () => {
+     menu.classList.remove('fa-times');
+     navbar.classList.remove('active');
+
+      if(window.scrollY > 0){
+         document.querySelector('.header').classList.add('active')
+      }else{
+        document.querySelector('.header').classList.remove('active');
+      }
+
+
+
+}
+
+
+
