f459b6e15e097ab241dc9d6c34f7f9524f338890
Pranaya Joshi WSC <joship@wsc.school.nz>
2026-08-30 16:42:23 +1200

    Sprint 4 and accessible on the phone

--- JS/script.js
+++ JS/script.js
@@ -44,3 +44,78 @@ tabs.forEach(tab => {
             .classList.add("active");
     });
 });
+
+// ---------------- Calorie Calculator ----------------
+
+let calculateButton = document.getElementById("calculate-btn");
+let calculatorResult = document.getElementById("calculator-result");
+calculateButton.addEventListener("click", calculateCalories);
+
+
+function calculateCalories() {
+
+    let age = Number(document.getElementById("age").value);
+    let gender = document.getElementById("gender").value;
+    let height = Number(document.getElementById("height").value);
+    let weight = Number(document.getElementById("weight").value);
+    let activity = Number(document.getElementById("activity").value);
+
+if (age === 0 || height === 0 || weight === 0 || gender === "" || activity === 0) {
+
+    calculatorResult.innerHTML = `
+        <div class="result-icon">
+            <i class="fas fa-exclamation"></i>
+        </div>
+
+        <p>
+            Please fill in all the details before calculating.
+        </p>
+    `;
+
+    return;
+}
+
+if (age < 15) {
+
+    calculatorResult.innerHTML = `
+        <div class="result-icon">
+            <i class="fas fa-exclamation"></i>
+        </div>
+
+        <p>
+            You must be at least 15 years old to use this calculator.
+        </p>
+    `;
+    return;
+}
+    let bmr;
+    if (gender === "male") {
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+    } else {
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+
+    }
+
+
+    let calories = bmr * activity;
+    calories = Math.round(calories);
+    calculatorResult.innerHTML = `
+
+        <div class="result-icon">
+        </div>
+
+        <p class="result-heading">
+            Your estimated daily energy needs are:
+        </p>
+
+        <div class="calorie-number">
+            ${calories} kcal/day
+        </div>
+
+        <p>
+            This is an estimate based on the information you entered.
+        </p>
+
+    `;
+}
+
